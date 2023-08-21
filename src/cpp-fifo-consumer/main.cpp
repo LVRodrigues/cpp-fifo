616d582155af84dfc1d97e32bef0aa38dd111391
LVRodrigues <lvrodriguesline@gmail.com>
2023-08-21 08:27:40 -0300

    Incializando o projeto.

---
+++ src/cpp-fifo-consumer/main.cpp
@@ -0,0 +1,8 @@
+#include <cstdlib>
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+    std::cout << "CPP FIFO Consumer" << std::endl;
+
+    return EXIT_SUCCESS;
+}
---
+++ src/cpp-fifo-producer/main.cpp
@@ -0,0 +1,8 @@
+#include <cstdlib>
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+    std::cout << "CPP FIFO Producer" << std::endl;
+
+    return EXIT_SUCCESS;
+}
